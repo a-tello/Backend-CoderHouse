65e749cbe4d40db1f2a373fdbc567658609af88f
a-tello <alee.e.tello@gmail.com>
2023-03-06 12:55:00 -0300

    Agrego desafio Server Express

---
+++ Desafio 3/ProductManager.js
@@ -0,0 +1,70 @@
+import fs from 'fs'
+
+class ProductManager {
+    constructor(path) {
+        this.path = path
+    }
+
+    async addProduct(product) {
+        if(Object.keys(product).length != 6) {
+            throw new Error('All fields are required!')
+        }
+
+        const products = await this.getProducts()
+
+        const existingProduct = this.#productExists(products, product.code) 
+        if (existingProduct) {
+            throw new Error('The product already exists')
+        }
+
+        const id = this.#generateId(products)
+        const newProduct = { id, ...product }
+
+        products.push(newProduct)
+        await fs.promises.writeFile(this.path, JSON.stringify(products))
+    }
+
+    async getProducts() {
+        if (fs.existsSync(this.path)) {
+            const products = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
+            return products
+        }
+        return []
+    }
+
+    async getProductById(productId) {
+        const products = await this.getProducts()
+        const product =  products.find((product) => product.id === productId)
+        
+        if (!product) throw new Error('Not found') 
+        return product
+    }
+
+    async updateProduct(productId, productValues) {
+        const products = await this.getProducts()
+        const productIndex = products.findIndex((product) => product.id === productId )
+        const updatedProduct = {...products[productIndex], ...productValues}
+
+        products.splice(productIndex, 1, updatedProduct)
+        await fs.promises.writeFile(this.path, JSON.stringify(products))
+    }
+
+    async deleteProducts(productId) {
+        const products = await this.getProducts()
+        const newProductList = products.filter((product) => product.id !== productId)
+
+        await fs.promises.writeFile(this.path, JSON.stringify(newProductList))
+    }
+
+    #productExists(products, productCode) {
+        return products.find((product) => product.code === productCode)
+    }
+
+    #generateId(products) {
+        return products.length
+            ? products[products.length - 1].id + 1
+            : 1
+    }
+}
+
+export default ProductManager
---
+++ Desafio 3/app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import  ProductManager from './ProductManager.js'
+
+
+const app = express()
+const productManager = new ProductManager('products.json')
+
+
+app.get('/', (req, res) => {
+    res.send('Hola')
+}
+)
+app.get('/products', async (req, res) => {
+    const products = await productManager.getProducts()
+    const limit = req.query.limit
+
+    if(!limit) res.send(products)
+    res.send(products.slice(0, limit))
+})
+
+app.get('/products/:pid', async (req, res) => {
+    const products = await productManager.getProducts()
+    const {pid} = req.params
+
+    if(!pid) res.send(products)
+    res.send(products.filter((prod) => prod.id === pid ))
+
+})
+
+app.listen(8080, () => {
+    console.log("Escuchando puerto 8080");
+})
