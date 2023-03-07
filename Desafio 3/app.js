f27335b60e378d523021c80a4a3b48767e267d47
a-tello <alee.e.tello@gmail.com>
2023-03-07 15:05:40 -0300

    Agrego gets

--- Desafio 3/app.js
+++ Desafio 3/app.js
@@ -1,27 +1,26 @@
 import express from 'express'
 import  ProductManager from './ProductManager.js'
 
-
 const app = express()
 const productManager = new ProductManager('products.json')
 
-
-app.get('/', (req, res) => {
-    res.send('Hola')
-}
-)
 app.get('/products', async (req, res) => {
     const products = await productManager.getProducts()
-    const limit = req.query.limit
+    const {limit} = req.query
 
-    if(!limit) res.send(products)
-    res.send(products.slice(0, limit))
+    if(!limit) return res.status(200).json(products)
+    res.status(200).json(products.slice(0, limit))
 })
 
 app.get('/products/:pid', async (req, res) => {
     const {pid} = req.params
-
-    res.json(await productManager.getProductById(+pid))
+        
+    try {
+        const product = await productManager.getProductById(+pid) 
+        res.status(200).json(product)
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    } 
 })
 
 app.listen(8080, () => {
