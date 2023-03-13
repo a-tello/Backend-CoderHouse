bc93d6b673698a92a5347d683973fd85a33ae43d
Alejandro Tello <alee.e.tello@gmail.com>
2023-03-13 19:22:03 -0300

    Creo archivo base del servidor

--- 4. Primera pre-entrega/src/app.js
+++ 4. Primera pre-entrega/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import productsRouter from './routes/products.router'
+import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
 
 const app = express()
 
-app.use('/products', productsRouter)
-app.use('/carts', cartsRouter)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
 
 app.listen(8080, () => {
     console.log('Listen 8080');
