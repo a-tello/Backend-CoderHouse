c1b1042df1dc49544c6338e277ac6ce935e97cf9
a-tello <alee.e.tello@gmail.com>
2023-03-21 12:10:11 -0300

    set socketServer

--- Desafio 4/src/app.js
+++ Desafio 4/src/app.js
@@ -1,19 +1,24 @@
 import express from 'express'
 import { __dirname } from './utils.js'
 import handlebars from 'express-handlebars'
-import viewsRouter from '../routes/views.router.js'
+import viewsRouter from './routes/views.router.js'
+import { Server } from 'socket.io'
 
 const PORT = 8080
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(__dirname + '/public' ))
+app.use(express.static(__dirname + '/public'))
 
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars') 
 
-app.listen(PORT, () => {
+app.use('/views',viewsRouter)
+
+const httpServer = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
+
+const socketServer = new Server(httpServer)
