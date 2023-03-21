bc135fc49819f574dcaf1737343ab486178b3e86
a-tello <alee.e.tello@gmail.com>
2023-03-21 13:53:01 -0300

    changed route name

--- Desafio 4/src/app.js
+++ Desafio 4/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { __dirname } from './utils.js'
 import handlebars from 'express-handlebars'
+import homeRouter from './routes/home.router.js'
 import viewsRouter from './routes/views.router.js'
 import { Server } from 'socket.io'
 
@@ -15,10 +16,14 @@ app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars') 
 
-app.use('/views',viewsRouter)
+app.use('/realtimeproducts',viewsRouter)
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
 
 const socketServer = new Server(httpServer)
+
+socketServer.on('connection', () => {
+    console.log('Cliente conectado');
+})
