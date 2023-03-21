e35c71626655976133640f2dce3c529dc0cdd672
a-tello <alee.e.tello@gmail.com>
2023-03-21 10:56:59 -0300

    added handlebars configuration

--- Desafio 4/src/app.js
+++ Desafio 4/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
+import viewsRouter from '../routes/views.router.js'
 
 const PORT = 8080
 const app = express()
@@ -7,6 +9,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/public' ))
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars') 
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
