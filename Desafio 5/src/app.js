3b221909a9e9cb72ccdb1b3a8a4554eff64cd656
a-tello <alee.e.tello@gmail.com>
2023-04-04 15:20:13 -0300

    added server config

---
+++ Desafio 5/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
+import '.db/dbConfig.js'
+const PORT = 8080
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.static(__dirname + '/public'))
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
