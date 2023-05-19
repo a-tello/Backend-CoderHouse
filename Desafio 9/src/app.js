d5c705b70be71672fb7fb108e0beacf771117109
a-tello <alee.e.tello@gmail.com>
2023-05-19 12:41:20 -0300

    changed server structure

--- Desafio 9/src/app.js
+++ Desafio 9/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { __dirname } from './utils.js'
 import handlebars from 'express-handlebars'
-import './db/dbConfig.js'
+import './DAL/dbConfig.js'
 import './config.js'
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
