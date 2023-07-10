c9d0e325883a306dd6ef972ef4479339e8412b5e
a-tello <alee.e.tello@gmail.com>
2023-07-10 08:42:35 -0300

    first commit

---
+++ Desafio 13/src/app.js
@@ -0,0 +1,99 @@
+import express from 'express'
+import handlebars from 'express-handlebars'
+import config from './config.js'
+import { __dirname } from './utils.js'
+import './DAL/mongoDB/dbConfig.js'
+
+import productsRouter from './routes/products.router.js'
+import cartsRouter from './routes/carts.router.js'
+import chatRouter from './routes/chat.router.js'
+import viewsRouter from './routes/views.router.js'
+import usersRouter from './routes/users.router.js'
+import sessionsRouter from './routes/sessions.router.js'
+import testRouter from './routes/test.router.js'
+
+import { Server } from 'socket.io'
+import MessageManager from './DAL/DAO/messagesManagerMongo.js'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import mongoStore from 'connect-mongo'
+import passport from 'passport'
+import cors from 'cors'
+import './passport/passportStrategies.js'
+import { generateProduct } from './utils/mocks.js'
+import { errorMiddleware } from './errors/error.middleware.js'
+import { logger } from './winston.js'
+
+const PORT = config.port
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+//app.use(cors({origin:'localhost:8080',allowedHeaders:true}))
+app.use(cors())
+
+app.use(express.static(__dirname + '/public'))
+
+app.engine('handlebars', handlebars.engine(
+    {
+        helpers:{
+            link: (url, variable) => `${url}${variable}`,
+            link_purchase: (url, variable) => `${url}${variable}/purchase`
+        }
+    }))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+
+app.use(
+    session({
+        secret: 'sessionKey',
+        resave: false,
+        saveUninitialized: true,
+        store: new mongoStore({
+            mongoUrl: config.mongo_URI
+        })
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+    res.redirect('/views/login')
+})
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+app.use('/api/chat', chatRouter)
+app.use('/views', viewsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/sessions', sessionsRouter)
+app.use('/test', testRouter)
+
+app.use(errorMiddleware)
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
+
+const socketServer = new Server(httpServer)
+const messageManager = new MessageManager()
+
+socketServer.on('connection', async (socket) => {
+    console.log(`${socket.id} connected`)
+
+    socket.on('message', async info => {
+        await messageManager.addMessage(info)
+        const updatedMessages = await messageManager.getMessages()
+        socketServer.emit('chat', updatedMessages)
+    })
+
+    socket.on('userConnected', username => {
+        socket.broadcast.emit('alertUserConnected', username)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected`)
+    })
+
+})
---
+++ Desafio 13/src/routes/carts.router.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { addEmptyCart, addOneProductToCart, addProducts, deleteProduct, emptyCart, getOneCart, purchase, updateProductsFromCart } from '../controllers/carts.controller.js'
+import { jwtValidator, verifyTokenPremiumOrUser, verifyTokenUser } from '../middleware/jwt.middleware.js'
+
+const router = Router()
+
+router.get('/:cid', getOneCart)
+router.get('/:cid/purchase', jwtValidator, purchase)
+router.post('/', addEmptyCart)
+router.post('/:cid/product/:pid', verifyTokenPremiumOrUser,addOneProductToCart)
+router.put('/:cid', addProducts)
+router.put('/:cid/products/:pid',updateProductsFromCart )
+router.delete('/:cid/products/:pid', deleteProduct)
+router.delete('/:cid', emptyCart)
+
+export default router
