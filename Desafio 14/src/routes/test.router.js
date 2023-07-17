94b6e3f381df547e8dc3de622228c2f6a05c7d2e
a-tello <alee.e.tello@gmail.com>
2023-07-17 10:44:50 -0300

    First commit

---
+++ Desafio 14/src/routes/test.router.js
@@ -0,0 +1,26 @@
+import { Router } from 'express'
+import { logger } from '../winston.js'
+import { generateProduct } from '../utils/mocks.js'
+
+const router = Router()
+
+router.get('/mockingproducts', (req, res) => {
+    try {
+        const mockProducts = generateProduct(100)
+        res.json({message:'Products created', products: mockProducts})
+    } catch (error) {
+        throw error
+    }
+})
+router.get('/loggerTest', (req,res) => {
+    logger.fatal(`Method: ${req.method} - URL: ${req.url}`)
+    logger.error(`Method: ${req.method} - URL: ${req.url}`)
+    logger.warning(`Method: ${req.method} - URL: ${req.url}`)
+    logger.info(`Method: ${req.method} - URL: ${req.url}`)
+    logger.http(`Method: ${req.method} - URL: ${req.url}`)
+    logger.debug(`Method: ${req.method} - URL: ${req.url}`)
+    res.render('test', {message:'Testeando logger'})
+})
+
+
+export default router
---
+++ Desafio 14/src/winston.js
@@ -0,0 +1,63 @@
+import winston from 'winston'
+import config from './config.js'
+
+const env = config.node_env || 'production'
+
+const customLevels = {
+    names: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        http: 4,
+        debug: 5,
+    },
+    colors: {
+        fatal: 'red',
+        error: 'red',
+        warning: 'yellow',
+        info: 'blue',
+        http: 'magenta',
+        debug: 'green',
+    }
+}
+
+export let logger
+
+if(env === 'development') {
+    console.log('development');
+    logger = winston.createLogger({
+        levels: customLevels.names,
+        level: 'debug',
+        transports: [
+            new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize({colors:customLevels.colors}),
+                    winston.format.simple()
+                )
+            })
+        ]
+    })
+} else {
+    logger = winston.createLogger({
+        levels: customLevels.names,
+        level: 'info',
+        transports: [
+            new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize({colors:customLevels.colors}),
+                    winston.format.simple()
+                )
+            }),
+            new winston.transports.File({
+                level: 'error',
+                filename: 'errors.log',
+                format: winston.format.combine(
+                    winston.format.timestamp(),
+                    winston.format.prettyPrint()
+                )
+            })
+        ]
+    })
+}
+
